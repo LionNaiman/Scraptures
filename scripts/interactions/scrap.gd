fd321cb4d5544aae715c5d68c16542c838b69b03
LionNaiman <lion11518@gmail.com>
2026-07-28 13:47:57 +0300

    updated begning session 28/7 13:47

--- scripts/interactions/scrap.gd
+++ scripts/interactions/scrap.gd
@@ -1,6 +1,11 @@
+class_name Scrap #we define an new class named scrap
 extends Area2D
 
+#this line defines a signal that belongs to scrap-
+#u can think of a signal like a messege that says:"i have been collected"
+signal collected 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void: #this function recibed a signal from a body that entered its polygon
 	if body.is_in_group("player"):
+		collected.emit() ## this is the coommand which sends the signal
 		queue_free()
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,42 @@
 extends Node2D
+#@oneready - initiatlize the variable only once the node and the child exist
+#$ScrapLabel - find the child of the current node named scrapLabel
+@onready var scrap_label: Label = $ScrapLabel
+@onready var goal_label: Label = $GoalLabel
+var scrap_count: int = 0
+@export var scrap_goal: int = 3
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Scraptures development environment is ready.")
+	connect_scrap_signals() #calls one time this func once we enter the scene 
+	update_scrap_label() #display the label text right away
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+func connect_scrap_signals() -> void:
+	var scrap_nodes: Array[Node] = get_tree().get_nodes_in_group("scrap") #asks the scene tree all the nodes in this group
+
+	for node: Node in scrap_nodes:
+		var scrap: Scrap = node as Scrap
+
+		if scrap != null:
+			scrap.collected.connect(_on_scrap_collected) #connects each scrap to the scrap collection method 
+
+
+func _on_scrap_collected() -> void:
+	scrap_count += 1
+	update_scrap_label() #call the display function 
+	check_scrap_goal()
+	print("Scrap collected. Total: ", scrap_count)
+
+func update_scrap_label() -> void: #a function which dispalyes the text of the label 
+	scrap_label.text = (
+		"Scrap: "
+		+ str(scrap_count)
+		+ " / "
+		+ str(scrap_goal)
+	)
+#a function to check if we have reached our goal
+func check_scrap_goal() -> void:
+	if scrap_count >= scrap_goal:
+		goal_label.text = "All Scrap Collected!"
